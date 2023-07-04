fc1849500af512e791ed7d5cf84de0e09047697c
Brendan Taylor <bct@diffeq.com>
2023-07-04 07:31:43 -0600

    add -add-rpc argument

--- cmd/subsonic-action-proxy/subsonic-action-proxy.go
+++ cmd/subsonic-action-proxy/subsonic-action-proxy.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"github.com/kballard/go-shellquote"
 	"log"
@@ -12,12 +13,13 @@ import (
 	"strings"
 )
 
-type commands []command
 type command []string
 
 func (cmd command) String() string {
 	return shellquote.Join(cmd...)
 }
+
+type commands []command
 
 func (cmds commands) String() string {
 	var strs []string
@@ -37,6 +39,41 @@ func (cmds *commands) Set(value string) error {
 	return nil
 }
 
+type rpc struct {
+	path string
+	cmd  command
+}
+
+func (rpc rpc) String() string {
+	return rpc.path + " -> " + rpc.cmd.String()
+}
+
+type rpcs []rpc
+
+func (rpcs rpcs) String() string {
+	var strs []string
+	for _, rpc := range rpcs {
+		strs = append(strs, rpc.String())
+	}
+	return strings.Join(strs, ", ")
+}
+
+func (rpcs *rpcs) Set(value string) error {
+	split := strings.SplitN(value, " ", 2)
+	if len(split) != 2 {
+		return errors.New("-add-rpc arguments should have the form \"path command\"")
+	}
+
+	path := split[0]
+	cmd, err := shellquote.Split(split[1])
+	if err != nil {
+		return err
+	}
+
+	*rpcs = append(*rpcs, rpc{path: path, cmd: cmd})
+	return nil
+}
+
 func executeCommand(cmd command) {
 	log.Printf("running %q", cmd.String())
 
@@ -48,6 +85,18 @@ func executeCommand(cmd command) {
 
 func isJukeboxControlSet(r *http.Request) bool {
 	return r.URL.Path == `/rest/jukeboxControl.view` && r.FormValue("action") == "set"
+}
+
+func RpcRequestHandler(rpc rpc) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			go executeCommand(rpc.cmd)
+
+			// 200 OK with no body.
+		} else {
+			http.Error(w, "Invalid request method.", 405)
+		}
+	}
 }
 
 func ProxyRequestHandler(proxy *httputil.ReverseProxy, jukeboxSetCommands commands) func(http.ResponseWriter, *http.Request) {
@@ -67,7 +116,10 @@ func main() {
 	listenAddr := flag.String("listen-addr", "0.0.0.0:8080", "listen address")
 
 	var jukeboxSetCommands commands
-	flag.Var(&jukeboxSetCommands, "jukebox-set-command", "command to run when jukeboxControl 'set' is called (can be specified multiple times)")
+	flag.Var(&jukeboxSetCommands, "jukebox-set-command", "command to run when jukeboxControl 'set' is called\n(can be specified multiple times)")
+
+	var rpcs rpcs
+	flag.Var(&rpcs, "add-rpc", "form: \"path command\", e.g. \"/rpc/volume-up /bin/volume.sh +10\".\nregisters a command to that will be run on a POST request to the given path.\n(can be specified multiple times)")
 
 	flag.Parse()
 
@@ -83,6 +135,11 @@ func main() {
 	proxy := httputil.NewSingleHostReverseProxy(subsonicUrl)
 
 	http.HandleFunc("/", ProxyRequestHandler(proxy, jukeboxSetCommands))
+
+	for i := range rpcs {
+		rpc := rpcs[i]
+		http.HandleFunc(rpc.path, RpcRequestHandler(rpc))
+	}
 
 	log.Printf("Starting server on %q", *listenAddr)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
